bfd5b4e9c306cd49fe707471c5d140736436f81c
Crystal Bajgai <crystalbajgai@gmail.com>
2023-11-15 19:23:18 +0545

    Create function to add task.

---
+++ todo.py
@@ -0,0 +1,25 @@
+"""
+Allow the user to add tasks to a to-do list.
+Allow the user to view their current to-do list.
+Allow the user to mark tasks as completed and remove them from the list.
+"""
+task = []
+
+
+def addTask():
+    while True:
+        userin = input("Add a task: ")
+        task.append(userin)
+
+        while True:
+            prompt = input("Do you want to add more task (y/n): ")
+            if prompt.upper() == "N":
+                return
+            elif prompt.upper() == "Y":
+                break
+            else:
+                print("The input must be either (y/n): ")
+
+
+addTask()
+print(task)
