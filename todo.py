86bb26a9fd928cc3d7a150b95aa50f301d47d07a
Crystal Bajgai <crystalbajgai@gmail.com>
2023-11-16 17:37:31 +0545

    Improve addTask and implement printTask function to view the tasks.

--- todo.py
+++ todo.py
@@ -9,31 +9,48 @@ task = []
 print("Welcome to the To-Do List App!\n")
 
 
+def addTask():
+    with open("./tasks.txt", "a") as f:
+        while True:
+            userin = input("Add a task: ")
+            task.append(userin)
+            f.write(userin + "\n")
+            print("Tasks saved.")
+
+            while True:
+                prompt = input("Do you want to add more task (y/n): ")
+                if prompt.upper() == "N":
+                    return
+                elif prompt.upper() == "Y":
+                    break
+                else:
+                    print("The input must be either (y/n).")
+
+
+def main():
+    print("Please select one of the following options: ")
+    print("1. View to-do list")
+    print("2. Add task to to-do list")
+    print("3. Mark task as completed")
+    print("4. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        printTasks()
+    elif choice == "2":
+        addTask()
+    # elif choice == "3":
+    # print("Mark task as completed")
+    # elif choice == "4":
+    # print("Exit")
+    else:
+        print("Invalid input. Please try again.")
+
+
 def printTasks():
     with open("./tasks.txt", "r") as f:
-        print(f.read())
+        lines = f.readlines()
+        for line in lines:
+            print(line.rstrip())
 
 
-def addTask():
-    while True:
-        userin = input("Add a task: ")
-        task.append(userin)
-
-        while True:
-            prompt = input("Do you want to add more task (y/n): ")
-            if prompt.upper() == "N":
-                return
-            elif prompt.upper() == "Y":
-                break
-            else:
-                print("The input must be either (y/n).")
-
-
-def listToString(task):
-    return "\n".join(task) + "\n"
-
-
-with open("./tasks.txt", "a") as f:
-    f.write(listToString(task))
-
-print("Tasks saved.")
+main()
